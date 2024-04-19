fa7d5e1816d5523e63c8be30ce600236bad046d6
Ming-Kit Choy <samuelmkchoy@hotmail.com>
2024-04-19 10:53:06 +0100

    update

--- lib/adminPage.dart
+++ lib/adminPage.dart
@@ -3,7 +3,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:fyp2/game/hashi/inputPuzzle.dart';
 import 'package:intl/intl.dart';
 
 class AdminPage extends StatelessWidget {
@@ -239,6 +238,55 @@ class _EditFeedbackPageState extends State<EditFeedbackPage> {
                     .pop(); // Navigate back to the admin feedback page
               },
               child: Text('Save Changes'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class InputPuzzlePage extends StatelessWidget {
+  final TextEditingController puzzleController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Input Puzzle'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: puzzleController,
+              decoration: InputDecoration(
+                labelText: 'Enter puzzle data',
+                hintText: 'e.g.: 4, 0, 3, 0, 2; 4, 3..',
+              ),
+              maxLines: null, // Allow multiline input
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                // Get the entered puzzle data
+                String puzzleData = puzzleController.text;
+                // Split the puzzle data into individual rows
+                List<String> rows = puzzleData.split(';');
+                // Trim each row to remove leading and trailing spaces
+                rows = rows.map((row) => row.trim()).toList();
+                // Add puzzleData to Firebase
+                await FirebaseFirestore.instance.collection('puzzleData').add({
+                  'puzzle': rows,
+                });
+                // Show success message
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text('Puzzle data added successfully!'),
+                ));
+              },
+              child: Text('Submit Puzzle'),
             ),
           ],
         ),
